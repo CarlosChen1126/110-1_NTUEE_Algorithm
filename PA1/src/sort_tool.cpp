6097245dc8ef769f8c42ac991a1ce9aaa5d7925f
carloschen1126 <carlos79103@gmail.com>
2021-10-05 21:09:36 +0800

    MS 5case OK

---
+++ PA1/src/sort_tool.cpp
@@ -0,0 +1,144 @@
+// **************************************************************************
+//  File       [sort_tool.cpp]
+//  Author     [Yu-Hao Ho]
+//  Synopsis   [The implementation of the SortTool Class]
+//  Modify     [2020/9/15 Mu-Ting Wu]
+// **************************************************************************
+
+#include "sort_tool.h"
+#include <cmath>
+using std::floor;
+#include <iostream>
+#include <vector>
+using std::vector;
+// Constructor
+SortTool::SortTool() {}
+
+// Insertsion sort method
+void SortTool::InsertionSort(vector<int> &data)
+{
+    // Function : Insertion sort
+    // TODO : Please complete insertion sort code here
+}
+
+// Quick sort method
+void SortTool::QuickSort(vector<int> &data)
+{
+    QuickSortSubVector(data, 0, data.size() - 1);
+}
+// Sort subvector (Quick sort)
+void SortTool::QuickSortSubVector(vector<int> &data, int low, int high)
+{
+    // Function : Quick sort subvector
+    // TODO : Please complete QuickSortSubVector code here
+    // Hint : recursively call itself
+    //        Partition function is needed
+}
+
+int SortTool::Partition(vector<int> &data, int low, int high)
+{
+    // Function : Partition the vector
+    // TODO : Please complete the function
+    // Hint : Textbook page 171
+}
+
+// Merge sort method
+void SortTool::MergeSort(vector<int> &data)
+{
+    MergeSortSubVector(data, 0, data.size() - 1);
+}
+
+// Sort subvector (Merge sort)
+void SortTool::MergeSortSubVector(vector<int> &data, int low, int high)
+{
+    // Function : Merge sort subvector
+    // TODO : Please complete MergeSortSubVector code here
+    // Hint : recursively call itself
+    //        Merge function is needed
+    if ((high - low) >= 1)
+    {
+        int middle1 = floor((low + high) / 2);
+        int middle2 = middle1 + 1;
+        MergeSortSubVector(data, low, middle1);
+        MergeSortSubVector(data, middle2, high);
+        Merge(data, low, middle1, middle2, high);
+    }
+}
+
+// Merge
+void SortTool::Merge(vector<int> &data, int low, int middle1, int middle2, int high)
+{
+    // Function : Merge two sorted subvector
+    // TODO : Please complete the function
+    int left = low;
+    int right = middle2;
+    int combineindex = low;
+    vector<int> combined(data.size());
+    while (left <= middle1 && right <= high)
+    {
+        if (data[left] < data[right])
+        {
+            combined[combineindex] = data[left];
+            left++;
+            combineindex++;
+        }
+        else
+        {
+            combined[combineindex] = data[right];
+            right++;
+            combineindex++;
+        }
+    }
+    if (left == middle2)
+    {
+        while (right <= high)
+        {
+            combined[combineindex] = data[right];
+            combineindex++;
+            right++;
+        }
+    }
+    else
+    {
+        while (left <= middle1)
+        {
+            combined[combineindex] = data[left];
+            combineindex++;
+            left++;
+        }
+    }
+    for (int i = low; i < combineindex; i++)
+    {
+        data[i] = combined[i];
+    }
+}
+
+// Heap sort method
+void SortTool::HeapSort(vector<int> &data)
+{
+    // Build Max-Heap
+    BuildMaxHeap(data);
+    // 1. Swap data[0] which is max value and data[i] so that the max value will be in correct location
+    // 2. Do max-heapify for data[0]
+    for (int i = data.size() - 1; i >= 1; i--)
+    {
+        swap(data[0], data[i]);
+        heapSize--;
+        MaxHeapify(data, 0);
+    }
+}
+
+//Max heapify
+void SortTool::MaxHeapify(vector<int> &data, int root)
+{
+    // Function : Make tree with given root be a max-heap if both right and left sub-tree are max-heap
+    // TODO : Please complete max-heapify code here
+}
+
+//Build max heap
+void SortTool::BuildMaxHeap(vector<int> &data)
+{
+    heapSize = data.size(); // initialize heap size
+    // Function : Make input data become a max-heap
+    // TODO : Please complete BuildMaxHeap code here
+}
