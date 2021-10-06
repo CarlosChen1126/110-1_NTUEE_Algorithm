1976b321b03767e837720d7159b2c125e12fa565
carloschen1126 <carlos79103@gmail.com>
2021-10-06 13:42:14 +0800

    QS OK

--- PA1/src/sort_tool.cpp
+++ PA1/src/sort_tool.cpp
@@ -42,6 +42,12 @@ void SortTool::QuickSortSubVector(vector<int> &data, int low, int high)
     // TODO : Please complete QuickSortSubVector code here
     // Hint : recursively call itself
     //        Partition function is needed
+    if (low < high)
+    {
+        int middle = Partition(data, low, high);
+        QuickSortSubVector(data, low, middle - 1);
+        QuickSortSubVector(data, middle + 1, high);
+    }
 }
 
 int SortTool::Partition(vector<int> &data, int low, int high)
@@ -49,6 +55,18 @@ int SortTool::Partition(vector<int> &data, int low, int high)
     // Function : Partition the vector
     // TODO : Please complete the function
     // Hint : Textbook page 171
+    int x = data[high];
+    int i = low - 1;
+    for (int j = low; j <= high - 1; j++)
+    {
+        if (data[j] <= x)
+        {
+            i++;
+            swap(data[i], data[j]);
+        }
+    }
+    swap(data[i + 1], data[high]);
+    return i + 1;
 }
 
 // Merge sort method
